8244bb2d608ccaec8c56934b74ee803f611bd6fd
Roman <rmngorelov@gmail.com>
2024-06-05 14:28:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -256,7 +256,7 @@ document.addEventListener('DOMContentLoaded', () => {
             direction *= -1;
             enemies.forEach((enemy) => {
                 let enemyY = parseInt(enemy.style.top);
-                enemy.style.top = enemyY + 200 + 'px'; // Increase this value to make the enemies fall faster
+                enemy.style.top = enemyY + 150 + 'px'; // Increase this value to make the enemies fall faster
             });
         }
 
